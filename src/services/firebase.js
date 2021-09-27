04e8906e5bdea2198181a4170cb0f10149b1d9f8
yosael <yosael.gutierrez@gmail.com>
2021-09-27 12:23:50 -0600

    add firebase config

---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const app = initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+});
+
+export const auth = getAuth();
+export default app;
