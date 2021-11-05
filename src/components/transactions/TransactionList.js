60cce1ddc4d4c8b2330c244ab985abd8151ba426
yosael <yosael.gutierrez@gmail.com>
2021-11-05 17:59:25 -0600

    List transactions by user

--- src/components/transactions/TransactionList.js
+++ src/components/transactions/TransactionList.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React,{ useContext,useState,useEffect } from "react";
 import { Table } from "react-bootstrap";
+import AuthContext from "../../context/auth-context";
+import axiosApi from "../../util/axios";
 
 const TransacctionList = ({ title }) => {
+
+  const authCtx = useContext(AuthContext);
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+
+    const getTransactions = async () => {
+      try {
+        console.log("sending user id: ",authCtx.currentUser.uid);
+        const response = await axiosApi.get(`/transactions/${authCtx.currentUser.uid}`);
+        console.log("getTransactions Response: ",response);
+        setTransactions(response.data.transactions);
+
+      } catch (error) {
+        console.log(error);
+      }
+      
+    }
+
+    getTransactions();
+    
+  }, []);
+
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>Account Name</th>
-          <th>Current Balance</th>
+          <th>Category</th>
+          <th>Amount</th>
+          <th>Date</th>
           <th>Type</th>
         </tr>
       </thead>
       <tbody>
-        <tr >
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
+        {
+          transactions &&
+          transactions.map((data)=> 
+            <tr key={data.transaction_id} >
+              <td>{data.category_name}</td>
+              <td>{data.amount}</td>
+              <td>{data.created_date}</td>
+              <td>{data.type}</td>
+            </tr>
+          )
+        }
+        
       </tbody>
     </Table>
   );
